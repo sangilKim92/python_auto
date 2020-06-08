ec2cdd2a63eb39e732045b24a1d961193b4079e0
sangil Kim <ksl13579@hanmail.net>
2020-06-08 20:05:06 +0900

    깃 자동화 완료

---
+++ auto_gitpush.py
@@ -0,0 +1,50 @@
+import pyautogui
+import time
+
+gitRepo=input('주소위치를 입력하시오')
+pyautogui.moveTo(356,1056)
+
+pyautogui.click()
+
+time.sleep(1)
+
+pyautogui.typewrite('git bash')
+time.sleep(1)
+pyautogui.typewrite(['enter'])
+
+time.sleep(3)
+
+pyautogui.typewrite('cd ')
+pyautogui.typewrite(gitRepo)
+pyautogui.typewrite(['enter'])
+
+time.sleep(1)
+pyautogui.typewrite('git add .')
+pyautogui.typewrite(['enter'])
+
+time.sleep(4)
+pyautogui.moveTo(502,404)
+pyautogui.click()
+
+pyautogui.typewrite('git commit -m "')
+gitComment=input('')
+pyautogui.typewrite('"')
+pyautogui.typewrite(['enter'])
+
+time.sleep(4)
+pyautogui.typewrite('git push -u origin master')
+pyautogui.typewrite(['enter'])
+
+time.sleep(4)
+pyautogui.typewrite('sangilKim92')
+pyautogui.typewrite(['enter'])
+
+time.sleep(3)
+pyautogui.typewrite('VLsbfX39pWjHxW')
+pyautogui.typewrite(['enter'])
+
+
+
+
+
+
